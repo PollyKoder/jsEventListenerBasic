790d19d24407b590360cb926b255260f1bc29654
Polly <riviera.koder@gmail.com>
2024-06-19 14:56:30 +0200

    jsDropdownMenu

--- script.js
+++ script.js
@@ -1,14 +1,23 @@
 //collecting the html elements to make function events
 const buttonToggle = document.querySelector(".toggle");
-
+const colorTheme = document.getElementById("themeChange");
 const buttonMenu = document.querySelector(".dropDown");
 
-//function to toggle the themes
-function themeToggle() {
+
+//function to toggle the themes and replacing button
+// function themeToggle() {
+// }
+colorTheme.addEventListener("click", () => {
     const bodyTheme = document.querySelector(".themeOne");
-    bodyTheme.classList.toggle(".themeTwo");
-}
+    bodyTheme.classList.toggle("themeTwo");
+});
 
-buttonToggle.addEventListener("click", themeToggle);
+buttonMenu.addEventListener("click", () => {
+    const display = document.querySelector(".hidden");
+    display.classList.toggle("showMenu");
+})
 
-console.log(themeToggle);
+
+
+
+
