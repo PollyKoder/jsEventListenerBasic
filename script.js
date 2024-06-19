2b71ee56fe030b9cd1588ad0b12daed679afdf2a
Polly <riviera.koder@gmail.com>
2024-06-19 14:07:06 +0200

    jsThemeEvent

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
 //collecting the html elements to make function events
 const buttonToggle = document.querySelector(".toggle");
-const buttonMenu = document.querySelector(".dropDown")
+
+const buttonMenu = document.querySelector(".dropDown");
+
+//function to toggle the themes
+function themeToggle() {
+    const bodyTheme = document.querySelector(".themeOne");
+    bodyTheme.classList.toggle(".themeTwo");
+}
+
+buttonToggle.addEventListener("click", themeToggle);
+
+console.log(themeToggle);
